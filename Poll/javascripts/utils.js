90e4aa6850bbe7e44c52aee90b5b4d51bdc8ea6f
emelie <emelie@earthpeople.se>
2019-02-13 12:26:55 +0100

    Utils with lang and other settings

---
+++ Poll/javascripts/utils.js
@@ -0,0 +1,44 @@
+const lang = {
+  heading: 'Vi lovar att förbättra...',
+  pledges: 'Löften',
+  categories: {
+    Clothing: 'Kläder',
+    Travels: 'Resor',
+    Purchases: 'Prylar',
+    Food: 'Mat',
+    Living: 'Bostaden'
+  }
+}
+
+const settings = {
+  Food: {
+    value: 1,
+    color: '#cedb29',
+    icon: 'food_icon.png'
+  },
+  Clothing: {
+    alue: 1,
+    color: '#5cb7b3',
+    icon: 'water_icon.png'
+  },
+  Travels: {
+    value: 1,
+    color: '#146170',
+    icon: 'transportation_icon.png'
+  },
+  Purchases: {
+    value: 1,
+    color: '#a65a95',
+    icon: 'habitat_icon.png'
+  },
+  Living: {
+    value: 1,
+    color: '#cb7d31',
+    icon: 'energy_icon.png'
+  }
+}
+
+module.exports = {
+  lang,
+  settings
+}
