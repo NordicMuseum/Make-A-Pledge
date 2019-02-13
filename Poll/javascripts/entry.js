b812e6b929ce975b6d1240845bb5734ec617c2da
emelie <emelie@earthpeople.se>
2019-02-13 12:27:28 +0100

    Sync categories with changes in backend

--- Poll/javascripts/entry.js
+++ Poll/javascripts/entry.js
@@ -1,85 +1,79 @@
-require('../less/main.less');
+import { PieChart, Pie, Cell } from 'recharts'
+import TransitionGroup from 'react-addons-transition-group'
+import { TweenMax } from 'gsap'
 
-import './index.css';
-import secret from './secrets.js';
+import './index.css'
+import secret from './secrets.js'
+import { lang, settings } from './utils.js'
 
-'use strict';
-
-var React = require('react');
-var ReactDOM = require('react-dom');
-import { PieChart, Pie, Sector, Cell, Label} from 'recharts';
-import TransitionGroup from 'react-addons-transition-group';
-import { TweenMax } from 'gsap';
+const React = require('react')
+const ReactDOM = require('react-dom')
 
 const data = [
-  {name: 'Water', value: 1, color: '#5cb7b3'},
-  {name: 'Habitat', value: 1, color: '#146170'},
-  {name: 'Transportation', value: 1, color: '#a65a95'},
-  {name: 'Food', value: 1, color: '#cb7d31'},
-  {name: 'Energy', value: 1, color: '#cedb29'}
-];
-
+  { name: 'Food', value: 1, color: '#cedb29' },
+  { name: 'Clothing', value: 1, color: '#5cb7b3' },
+  { name: 'Travels', value: 1, color: '#146170' },
+  { name: 'Purchases', value: 1, color: '#a65a95' },
+  { name: 'Living', value: 1, color: '#cb7d31' }
+]
 
 class Entry extends React.Component {
-
   GETInitialVotes () {
-
-    var myHeaders = new Headers({
-      'X-Api-Key' : secret,
-    }); 
-
-    const that = this;
-    var myInit = { method: 'GET', headers: myHeaders, mode: 'cors' };
-    return fetch('https://yhwyy8uzf7.execute-api.us-east-1.amazonaws.com/production/pledge', myInit)
-    .then(function(res) {
-
-      if (res.ok) { return res.json(); } 
-      else { throw new TypeError('GETVotes error'); }
-
+    const that = this
+    return fetch('https://58u3ew20yd.execute-api.us-east-1.amazonaws.com/dev/pledge', {
+      method: 'GET',
+      headers: new Headers({
+        'X-Api-Key': secret
+      }),
+      mode: 'cors'
     })
-    .then(function(resJSON) {
-      var total = 0;
+      .then(function (res) {
+        if (res.ok) {
+          return res.json()
+        } else {
+          throw new TypeError('GETVotes error')
+        }
+      })
+      .then(function (resJSON) {
+        let total = 0
 
-      const { currentVotes, iotProperties } = resJSON;
+        const { currentVotes, iotProperties } = resJSON
 
-      data.map((category, i) => {
-        var key = category.name;
-        var votes = currentVotes[key];
-        category.value = votes;
-        total += votes;
-        return category;
-      });
+        data.map((category, i) => {
+          let key = category.name
+          let votes = currentVotes[key]
+          category.value = votes
+          total += votes
+          return category
+        })
 
-      // Setup a websocket to listen to new votes
-      IoT.create(
-        iotProperties.topic,
-        iotProperties.endpoint,
-        iotProperties.region,
-        iotProperties.accessKey,
-        iotProperties.secretKey,
-        () => {
-          console.log('websocket connect');
-        }, 
-        (topic, message) => {
-          const { category, computerLocation } = JSON.parse(message);
-          that.animateVote(computerLocation, category);
-        }, 
-        () => {
-          console.log('websocket close');
-        });
-      
-      return { data, total };
+        // Setup a websocket to listen to new votes
+        IoT.create(
+          iotProperties.topic,
+          iotProperties.endpoint,
+          iotProperties.region,
+          iotProperties.accessKey,
+          iotProperties.secretKey,
+          () => {
+            console.log('websocket connect')
+          },
+          (topic, message) => {
+            const { category, computerLocation } = JSON.parse(message)
+            that.animateVote(computerLocation, category)
+          },
+          () => {
+            console.log('websocket close')
+          })
 
-    }).catch(function(err) {
-
-      console.log(err);
-    
-    });
+        return { data, total }
+      }).catch(function (err) {
+        console.log(err)
+      })
   }
 
   componentWillMount () {
-    this.renderLabel = this.renderLabel.bind(this);
-    this.GETInitialVotes = this.GETInitialVotes.bind(this);
+    this.renderLabel = this.renderLabel.bind(this)
+    this.GETInitialVotes = this.GETInitialVotes.bind(this)
 
     this.setState({
       total: 5,
@@ -88,22 +82,22 @@ class Entry extends React.Component {
       voteEnteringFromLeft: false,
       categoryEnteringFromLeft: null,
       voteEnteringFromRight: false,
-      categoryEnteringFromRight: null,
-    });
+      categoryEnteringFromRight: null
+    })
 
     this.GETInitialVotes().then(votes => {
       this.setState({
         total: votes.total,
-        data: votes.data,
-      });
-    });
+        data: votes.data
+      })
+    })
   }
 
   renderLabel (props) {
     return (
-      <text 
-        x={props.x > props.cx ? props.x+25 : props.x - 25}
-        y={props.y > props.cy ? props.y+25 : props.y - 25}
+      <text
+        x={props.x > props.cx ? props.x + 25 : props.x - 25}
+        y={props.y > props.cy ? props.y + 25 : props.y - 25}
         textAnchor={props.x > props.cx ? 'start' : 'end'}
         fill={'#fefaeb'}
         style={{
@@ -112,11 +106,15 @@ class Entry extends React.Component {
           fontFamily: 'BrandonBody',
           flexDirection: 'column',
           textAlign: 'center',
-          lineHeight: 1.5,
+          lineHeight: 1.5
         }}
       >
-          {this.state.showLabels && props.value > 0 ? props.name : ''}
-      </text>);
+        {this.state.showLabels && props.value > 0 ? this.translate(props.name) : ''}
+      </text>)
+  }
+
+  translate (name) {
+    return lang.categories[name] || ''
   }
 
   animateVote (direction, category) {
@@ -125,54 +123,56 @@ class Entry extends React.Component {
         voteEnteringFromLeft: true,
         categoryEnteringFromLeft: category,
         showLabels: false,
-      });
+      })
     } else {
       this.setState({
         voteEnteringFromRight: true,
         categoryEnteringFromRight: category,
         showLabels: false,
-      });
+      })
     }
   }
 
-  addVote(direction, category) {
-    var updatedData = this.state.data.map((entry, index) => {
+  addVote (direction, category) {
+    let updatedData = this.state.data.map((entry, index) => {
       if (entry.name === category) {
-        entry.value = entry.value + 1;
+        entry.value = entry.value + 1
       }
-      return entry;
+      return entry
     })
 
     if (direction === 'Left') {
       this.setState({
-        total: this.state.total+1,
+        total: this.state.total + 1,
         data: updatedData,
         categoryEnteringFromLeft: null,
-        voteEnteringFromLeft: false,
-      });
+        voteEnteringFromLeft: false
+      })
     } else {
       this.setState({
-        total: this.state.total+1,
+        total: this.state.total + 1,
         data: updatedData,
         categoryEnteringFromRight: null,
-        voteEnteringFromRight: false,
-      });
+        voteEnteringFromRight: false
+      })
     }
   }
 
   render () {
     return (
-      <div className="container">
-        <div 
+      <div className='container'>
+        <div
           style={{
             position: 'absolute',
             top: 0,
             left: 0,
             width: 1920,
             height: 1080,
+            background: '#000',
+            opacity: 0.3
           }}
         />
-        
+
         <text
           style={{
             position: 'absolute',
@@ -180,16 +180,16 @@ class Entry extends React.Component {
             left: 40,
             color: '#fefaeb',
             fontSize: 70,
-            fontFamily: 'BrandonTitle',
+            fontFamily: 'BrandonTitle'
           }}
-        > 
-          We pledge to protect...
+        >
+          {lang.heading}
         </text>
 
-        <text 
-          className="pledgeCount"
-          textAnchor="middle"
-          fill="#82ca9d"
+        <text
+          className='pledgeCount'
+          textAnchor='middle'
+          fill='#82ca9d'
           style={{
             color: '#fefaeb',
             fontSize: 90,
@@ -198,68 +198,70 @@ class Entry extends React.Component {
             textAlign: 'center',
             lineHeight: 1,
             position: 'absolute',
-            top: 1080/2 - 20,
-          }}> 
-          {this.state.total+'\n'}
-          <text style={{ fontSize: 60, display: 'block', lineHeight: 1.3 }}>Pledges</text>
+            top: 1080 / 2 - 20
+          }}>
+          {this.state.total + '\n'}
+          <text style={{ fontSize: 60, display: 'block', lineHeight: 1.3 }}>
+            {lang.pledges}
+          </text>
         </text>
 
         <PieChart
-          className="chart"
+          className='chart'
           width={1920}
           height={1080}
           style={{
             position: 'relative',
-            top: 50,
+            top: 50
           }}
         >
           <Pie
-            data={this.state.data} 
+            data={this.state.data}
             cx={'50%'}
             cy={'50%'}
             innerRadius={225}
             outerRadius={375}
-            fill="#82ca9d"
+            fill='#82ca9d'
             labelLine={false}
             label={this.renderLabel}
           >
             {
-              this.state.data.map((entry, index) => 
-                <Cell 
+              this.state.data.map((entry, index) =>
+                <Cell
                   key={index}
                   fill={entry.color}
                   stroke={'transparent'}
                 />
               )
             }
-          </Pie> 
-         </PieChart>
+          </Pie>
+        </PieChart>
 
         <TransitionGroup>
-          { this.state.voteEnteringFromLeft && 
-            <Vote 
+          { this.state.voteEnteringFromLeft &&
+            <Vote
               direction={'Left'}
-              enterCallback={() => { 
-                this.addVote('Left', this.state.categoryEnteringFromLeft); 
+              enterCallback={() => {
+                this.addVote('Left', this.state.categoryEnteringFromLeft)
               }}
               category={this.state.categoryEnteringFromLeft}
-              exitCallback={() => { 
-                // console.log('show those labels');
-                this.setState({ showLabels: true }); 
+              exitCallback={() => {
+                // console.log('show those labels')
+                this.setState({ showLabels: true })
               }}
             />
           }
         </TransitionGroup>
 
-        <TransitionGroup> 
-          { this.state.voteEnteringFromRight && 
-            <Vote 
+        <TransitionGroup>
+          { this.state.voteEnteringFromRight &&
+            <Vote
               direction={'Right'}
               enterCallback={() => {
-                this.addVote('Right', this.state.categoryEnteringFromRight);
+                this.addVote('Right', this.state.categoryEnteringFromRight)
               }}
               category={this.state.categoryEnteringFromRight}
-              exitCallback={() => { this.setState({ showLabels: true }); }}
+              exitCallback={() => { this.setState({ showLabels: true }) }}
             />
           }
         </TransitionGroup>
@@ -267,99 +269,111 @@ class Entry extends React.Component {
       </div>
     )
   }
-};
-
+}
 
 class Vote extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.enterAnimationComplete = this.enterAnimationComplete.bind(this);
-    this.exitAnimationComplete = this.exitAnimationComplete.bind(this);
+  constructor (props) {
+    super(props)
+    this.enterAnimationComplete = this.enterAnimationComplete.bind(this)
+    this.exitAnimationComplete = this.exitAnimationComplete.bind(this)
   }
 
   enterAnimationComplete (props) {
-    this.props.enterCallback();
+    this.props.enterCallback()
   }
 
   exitAnimationComplete (props) {
-    this.props.exitCallback();
+    this.props.exitCallback()
   }
 
   componentWillEnter (callback) {
-    const el = this.container;
-    const im = this.image;
-    TweenMax.fromTo(im, 1, {opacity: 1}, {opacity: 0});
-    TweenMax.fromTo(el, 1, {y: 500, opacity: 1}, {y: 0, opacity: 1, onComplete: () => {
-      this.enterAnimationComplete();
-      callback();
-    }});
+    const el = this.container
+    const im = this.image
+    TweenMax.fromTo(im, 1, { opacity: 1 }, { opacity: 0 })
+    TweenMax.fromTo(el, 1, { y: 500, opacity: 1 }, {
+      y: 0,
+      opacity: 1,
+      onComplete: () => {
+        this.enterAnimationComplete()
+        callback()
+      }
+    })
   }
 
   componentWillLeave (callback) {
-    const el = this.container;
+    const el = this.container
     if (this.props.direction === 'Left') {
-      TweenMax.fromTo(el, 2, {x: 0, opacity: 1, scale: 1 }, {x: 700, opacity: 1, scale: 0, onComplete: () => {
-        this.exitAnimationComplete();
-        callback();
-      }});
+      TweenMax.fromTo(el, 2, { x: 0, opacity: 1, scale: 1 }, {
+        x: 700,
+        opacity: 1,
+        scale: 0,
+        onComplete: () => {
+          this.exitAnimationComplete()
+          callback()
+        }
+      })
     } else {
-      TweenMax.fromTo(el, 2, {x: 0, opacity: 1, scale: 1 }, {x: -700, opacity: 1, scale: 0, onComplete: () => {
-        this.exitAnimationComplete();
-        callback();
-      }});
+      TweenMax.fromTo(el, 2, { x: 0, opacity: 1, scale: 1 }, {
+        x: -700,
+        opacity: 1,
+        scale: 0,
+        onComplete: () => {
+          this.exitAnimationComplete()
+          callback()
+        }
+      })
     }
   }
 
   render () {
-    return (<div
-      ref={c => this.container = c}
-      style={
-        this.props.direction === 'Left' ? {
+    return (
+      <div
+        ref={c => this.container = c}
+        style={
+          this.props.direction === 'Left' ? {
             position: 'absolute',
             top: 500,
             width: 150,
             height: 150,
             left: 200,
-            borderRadius: 75,
-          } : { 
+            borderRadius: 75
+          } : {
             position: 'absolute',
             top: 500,
             width: 150,
             height: 150,
             right: 200,
-            borderRadius: 75,
-        }
-      }
-      >
-        {this.props.category === 'Transportation' &&
-          <div style={{ backgroundColor: '#a65a95', borderRadius: 75 }}>
-            <img ref={c => this.image = c} style={{ flex: 1 }} src={require('./assets/transportation_icon.png')} /> 
+            borderRadius: 75
+          }
+        }>
+        {this.props.category === 'Purchases' &&
+          <div style={{ backgroundColor: settings['Purchases'].color, borderRadius: 75 }}>
+            <img ref={c => this.image = c} style={{ flex: 1 }} src={require(`./assets/${settings['Purchases'].icon}`)} />
           </div>
         }
-        {this.props.category === 'Habitat' &&
-          <div style={{ backgroundColor: '#146170', borderRadius: 75 }}>
-            <img ref={c => this.image = c} style={{ flex: 1 }} src={require('./assets/habitat_icon.png')} /> 
+        {this.props.category === 'Travels' &&
+          <div style={{ backgroundColor: settings['Travels'].color, borderRadius: 75 }}>
+            <img ref={c => this.image = c} style={{ flex: 1 }} src={require(`./assets/${settings['Travels'].icon}`)} />
           </div>
         }
-        {this.props.category === 'Water' &&
-          <div style={{ backgroundColor: '#5cb7b3', borderRadius: 75 }}>
-            <img ref={c => this.image = c} style={{ flex: 1 }} src={require('./assets/water_icon.png')} /> 
+        {this.props.category === 'Clothing' &&
+          <div style={{ backgroundColor: settings['Clothing'].color, borderRadius: 75 }}>
+            <img ref={c => this.image = c} style={{ flex: 1 }} src={require(`./assets/${settings['Clothing'].icon}`)} />
+          </div>
+        }
+        {this.props.category === 'Living' &&
+          <div style={{ backgroundColor: settings['Living'].color, borderRadius: 75 }}>
+            <img ref={c => this.image = c} style={{ flex: 1 }} src={require(`./assets/${settings['Living'].icon}`)} />
           </div>
         }
         {this.props.category === 'Food' &&
-          <div style={{ backgroundColor: '#cb7d31', borderRadius: 75 }}>
-            <img ref={c => this.image = c} style={{ flex: 1 }} src={require('./assets/food_icon.png')} /> 
+          <div style={{ backgroundColor: settings['Food'].color, borderRadius: 75 }}>
+            <img ref={c => this.image = c} style={{ flex: 1 }} src={require(`./assets/${settings['Food'].icon}`)} />
           </div>
         }
-        {this.props.category === 'Energy' &&
-          <div style={{ backgroundColor: '#cedb29', borderRadius: 75 }}>
-            <img ref={c => this.image = c} style={{ flex: 1 }} src={require('./assets/energy_icon.png')} /> 
-          </div>
-        }
-      </div>);
+      </div>
+    )
   }
 }
 
-
-ReactDOM.render(<Entry />, document.getElementById('content'));
+ReactDOM.render(<Entry />, document.getElementById('content'))
