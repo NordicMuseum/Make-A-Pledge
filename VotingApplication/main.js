43f873b77d2070ea1f119ee61b9b330419006f39
Aron Ambrosiani <aron@ambrosiani.se>
2019-05-27 14:47:17 +0200

    correct resolution

--- VotingApplication/main.js
+++ VotingApplication/main.js
@@ -13,7 +13,7 @@ var FromMeToWe = null;
 
 // CREATE WINDOWS
 app.on('ready', () => {
-    FromMeToWe = new BrowserWindow({ width: 1680, height: 1050, resizable: false, frame: false, fullscreen: true });
+    FromMeToWe = new BrowserWindow({ width: 1920, height: 1080, resizable: false, frame: false, fullscreen: true });
     FromMeToWe.loadURL(`file://${__dirname}/FromMeToWe.html`);
     FromMeToWe.setMenu(null);
 });
