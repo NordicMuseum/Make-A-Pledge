8d07d23393da6dcc327a25b715ba0d5134a4c3b6
Aron Ambrosiani <aron@ambrosiani.se>
2019-10-03 20:52:07 +0200

    Update categories.js

--- VotingApplication/data/categories.js
+++ VotingApplication/data/categories.js
@@ -9,7 +9,7 @@ var categories = {
         text: 'Klädindustrin och bomullsodling sliter hårt på miljön. Men handlar du second hand kan du spara upp till 97 procent av energin som går åt till att tillverka nya kläder. Det är dessutom roligt att gå på loppis där du kan hitta riktigt unika plagg till låga priser.',
         localOrgText: 'ExampleLocalOrg',
         localOrgLink: ['http://www.example.com/'],
-        learnMoreText: '<h2>Exempel ur utställningen</h2><img src="/make-a-pledge/images/objects/NM.0201803.jpg" height="190" class="learnMoreImage"/><p class="learnMoreText">Livtröja från Hälsingland, mitten av 1800-talet. Möjligen är bålen tagen från en äldre tröja som har skarvats i med annat ylletyg på axlarna. NM.0201803</p>',
+        learnMoreText: '<h2>Exempel ur Nordiska museets samlingar</h2><img src="/make-a-pledge/images/objects/NM.0201803.jpg" height="190" class="learnMoreImage"/><p class="learnMoreText">Livtröja från Hälsingland, mitten av 1800-talet. Möjligen är bålen tagen från en äldre tröja som har skarvats i med annat ylletyg på axlarna. NM.0201803</p>',
         learnMoreLink: 'http://www.tidningenhalsa.se/artiklar/kategori/20120420/klimatsmarta-handlar-second-hand/'
       },
       {
@@ -18,7 +18,7 @@ var categories = {
         text: 'Fundera ordentligt varje gång du köper nya kläder. Om du verkligen behöver köpa nya kläder, köp då färre plagg i bättre kvalité som håller länge.',
         localOrgText: 'ExampleLocalOrg',
         localOrgLink: ['http://www.example.com/'],
-        learnMoreText: '<h2>Exempel ur utställningen</h2><img src="/make-a-pledge/images/objects/NM.0273621A-B.jpg" height="190" class="learnMoreImage"/><p class="learnMoreText">Vinternäbbskor från Lappland, tidigt 1900-tal. NM.0273621A-B</p>',
+        learnMoreText: '<h2>Exempel ur Nordiska museets samlingar</h2><img src="/make-a-pledge/images/objects/NM.0273621A-B.jpg" height="190" class="learnMoreImage"/><p class="learnMoreText">Vinternäbbskor från Lappland, tidigt 1900-tal. NM.0273621A-B</p>',
         learnMoreLink: 'https://extra.orebro.se/klimatsmartvardag/klimatsmartatips/ekonomiochkonsumtion/skapaenhallbargarderob.4.6ad5e21514fe44c621c5044.html'
       },
       {
@@ -27,7 +27,7 @@ var categories = {
         text: 'Om ett plagg går sönder. Reparera det då. Ett hål på byxorna kan lappas, grenen kan förstärkas och skjortkragen vändas eller bytas ut. Kan du redan sy, så sätt igång. Annars kan du gå en kurs eller lämna in plagget för lagning. Ett lappat plagg blir unikt och ofta mycket vackert.',
         localOrgText: 'ExampleLocalOrg',
         localOrgLink: ['http://www.example.com/'],
-        learnMoreText: '<h2>Exempel ur utställningen</h2><img src="/make-a-pledge/images/objects/NM.0240612.jpg" height="190" class="learnMoreImage"/><p class="learnMoreText">Lappade timmerhuggarbyxor från Töre i Norrbotten, tidigt 1900-tal. NM.0240612</p>',
+        learnMoreText: '<h2>Exempel ur Nordiska museets samlingar</h2><img src="/make-a-pledge/images/objects/NM.0240612.jpg" height="190" class="learnMoreImage"/><p class="learnMoreText">Lappade timmerhuggarbyxor från Töre i Norrbotten, tidigt 1900-tal. NM.0240612</p>',
         learnMoreLink: 'https://karlavagen31.se/bli-klimatsmart-sa-vardar-du-dina-klader'
 
       }
@@ -43,7 +43,7 @@ var categories = {
         text: 'När det är möjligt, ta cykeln eller promenera istället för att ta bilen. Det förbättrar din hälsa och gör att du sparar pengar samtidigt som du hjälper klimatet.',
         localOrgText: 'ExampleLocalOrg',
         localOrgLink: ['http://www.example.com/'],
-        learnMoreText: '<h2>Exempel ur utställningen</h2><img src="/make-a-pledge/images/objects/NM.0328486A-D.jpg" height="190" class="learnMoreImage"/><p class="learnMoreText">Cykel, tillverkad i Stockholm 1960. NM.0328486A-D</p>',
+        learnMoreText: '<h2>Exempel ur Nordiska museets samlingar</h2><img src="/make-a-pledge/images/objects/NM.0328486A-D.jpg" height="190" class="learnMoreImage"/><p class="learnMoreText">Cykel, tillverkad i Stockholm 1960. NM.0328486A-D</p>',
         learnMoreLink: 'https://voxnadalen.naturskyddsforeningen.se/gron-guide/res-klimatsmart'
       },
       {
@@ -52,7 +52,7 @@ var categories = {
         text: 'Istället för att köra bil. Gå över till pendeltåg, buss eller tunnelbana.',
         localOrgText: 'ExampleLocalOrg',
         localOrgLink: ['http://www.example.com/'],
-        learnMoreText: '<h2>Exempel ur utställningen</h2><img src="/make-a-pledge/images/objects/NMA.0058040.jpg" height="190" class="learnMoreImage"/><p class="learnMoreText">Turistbuss i Holmajärvi, Norrbotten, 1946. NMA.0058040</p>',
+        learnMoreText: '<h2>Exempel ur Nordiska museets samlingar</h2><img src="/make-a-pledge/images/objects/NMA.0058040.jpg" height="190" class="learnMoreImage"/><p class="learnMoreText">Turistbuss i Holmajärvi, Norrbotten, 1946. NMA.0058040</p>',
         learnMoreLink: 'https://voxnadalen.naturskyddsforeningen.se/gron-guide/res-klimatsmart'
       },
       {
@@ -61,7 +61,7 @@ var categories = {
         text: 'När du ska åka längre eller åka på semester, åk då tåg istället för bil eller flyg. Tåget är bekvämt och är många gånger en upplevelse i sig. Resan blir en del av målet. Dessutom slipper du stressiga flygplatser, incheckning av bagage och säkerhetskontroller.',
         localOrgText: 'ExampleLocalOrg',
         localOrgLink: ['http://www.example.com/'],
-        learnMoreText: '<h2>Exempel ur utställningen</h2><img src="/make-a-pledge/images/objects/NMA.0046474.jpg" height="190" class="learnMoreImage"/><p class="learnMoreText">Lillehammer tågstation, 1939. Foto: Gunnar Lundh. NMA.0046474</p>',
+        learnMoreText: '<h2>Exempel ur Nordiska museets samlingar</h2><img src="/make-a-pledge/images/objects/NMA.0046474.jpg" height="190" class="learnMoreImage"/><p class="learnMoreText">Lillehammer tågstation, 1939. Foto: Gunnar Lundh. NMA.0046474</p>',
         learnMoreLink: 'https://voxnadalen.naturskyddsforeningen.se/gron-guide/res-klimatsmart'
       }
     ]
@@ -76,7 +76,7 @@ var categories = {
         text: 'Inred ditt hem klimatsmart! Behöver du möbler, leksaker till barnen, handla second hand. Gå på loppis, kolla på blocket eller gå på auktion. Du får ofta kvalitetsmöbler för lägre pris än nytillverkade av sämre kvalité.',
         localOrgText: 'ExampleLocalOrg',
         localOrgLink: ['http://www.example.com/'],
-        learnMoreText: '<h2>Exempel ur utställningen</h2><img src="/make-a-pledge/images/objects/NM.0328242.jpg" height="190" class="learnMoreImage"/><p class="learnMoreText">Smörkärna tillverkad i Gävle, 1910-tal. Köptes begagnad av en småländsk torparfamilj och användes under 1930- och 1940-tal. NM.0328242</p>',
+        learnMoreText: '<h2>Exempel ur Nordiska museets samlingar</h2><img src="/make-a-pledge/images/objects/NM.0328242.jpg" height="190" class="learnMoreImage"/><p class="learnMoreText">Smörkärna tillverkad i Gävle, 1910-tal. Köptes begagnad av en småländsk torparfamilj och användes under 1930- och 1940-tal. NM.0328242</p>',
         learnMoreLink: 'https://malin.elledecoration.se/att-inreda-hallbart'
       },
       {
@@ -85,7 +85,7 @@ var categories = {
         text: 'Har något hemma gått sönder? Laga själv, eller lämna in för lagning istället för att slänga och köpa nytt.',
         localOrgText: 'ExampleLocalOrg',
         localOrgLink: ['http://www.example.com/'],
-        learnMoreText: '<h2>Exempel ur utställningen</h2><img src="/make-a-pledge/images/objects/NM.0115566.jpg" height="190" class="learnMoreImage"/><p class="learnMoreText">Spräckt och lagad skål från Östergötland, innan 1910. NM.0115566</p>',
+        learnMoreText: '<h2>Exempel ur Nordiska museets samlingar</h2><img src="/make-a-pledge/images/objects/NM.0115566.jpg" height="190" class="learnMoreImage"/><p class="learnMoreText">Spräckt och lagad skål från Östergötland, innan 1910. NM.0115566</p>',
         learnMoreLink: 'https://malin.elledecoration.se/att-inreda-hallbart'
       },
       {
@@ -93,7 +93,7 @@ var categories = {
         name: 'Gör långsiktiga inköp.',
         text: 'Behöver du verkligen köpa nytt, välj då något av högre kvalité som håller längre och som är tillverkat i miljövänliga material. Om du köper en soffa kan du  undersöka om klädseln är avtagbar och tvättbar, eller om ett bord går att slipa ner.',
         localOrgLink: [],
-        learnMoreText: '<h2>Exempel ur utställningen</h2><img src="/make-a-pledge/images/objects/NM.0059952.jpg" height="190" class="learnMoreImage"/><p class="learnMoreText">Täljstenslampa från Grönland som eldas med sälfett, sent 1800-tal. Lampan höll länge och var jämförelsevis energisnål. NM.0059952</p>',
+        learnMoreText: '<h2>Exempel ur Nordiska museets samlingar</h2><img src="/make-a-pledge/images/objects/NM.0059952.jpg" height="190" class="learnMoreImage"/><p class="learnMoreText">Täljstenslampa från Grönland som eldas med sälfett, sent 1800-tal. Lampan höll länge och var jämförelsevis energisnål. NM.0059952</p>',
         learnMoreLink: 'https://malin.elledecoration.se/att-inreda-hallbart'
       }
     ]
@@ -108,7 +108,7 @@ var categories = {
         text: 'Spara på elen och uppvärmningen genom att täta springor i väggar och golv.',
         localOrgText: 'ExampleLocalOrg',
         localOrgLink: ['http://www.example.com/'],
-        learnMoreText: '<h2>Exempel ur utställningen</h2><img src="/make-a-pledge/images/objects/NM.0126345.jpg" height="190" class="learnMoreImage"/><p class="learnMoreText">Rana (vävt tygstycke) från Vittangi i Lappland, innan 1915. Tygstycken har använts för att täta eller för att trivas med lägre inomhustemperatur NM.0126345</p>',
+        learnMoreText: '<h2>Exempel ur Nordiska museets samlingar</h2><img src="/make-a-pledge/images/objects/NM.0126345.jpg" height="190" class="learnMoreImage"/><p class="learnMoreText">Rana (vävt tygstycke) från Vittangi i Lappland, innan 1915. Tygstycken har använts för att täta eller för att trivas med lägre inomhustemperatur NM.0126345</p>',
         learnMoreLink: 'https://www.folksam.se/tester-och-goda-rad/miljo-och-ansvar/bli-klimatsmart/bo-klimatsmart'
       },
       {
@@ -117,7 +117,7 @@ var categories = {
         text: 'När du badar i badkar går det åt 150 liter vatten. Energianvändningen för att värma det vattnet är 5,6 kWh. Jämför det med att ta en 5 minuters dusch då det kan gå åt 60 liter vatten som värms upp med 2,2 kWh. Unna dig gärna ett bad ibland, men gör det sällan. Det tjänar både du och miljön på.',
         localOrgText: 'ExampleLocalOrg',
         localOrgLink: ['http://www.example.com/'],
-        learnMoreText: '<h2>Exempel ur utställningen</h2><img src="/make-a-pledge/images/objects/NM.0319074.jpg" height="190" class="learnMoreImage"/><p class="learnMoreText">Tvål, tillverkad i Nyköping 1970. NM.0319074</p>',
+        learnMoreText: '<h2>Exempel ur Nordiska museets samlingar</h2><img src="/make-a-pledge/images/objects/NM.0319074.jpg" height="190" class="learnMoreImage"/><p class="learnMoreText">Tvål, tillverkad i Nyköping 1970. NM.0319074</p>',
         learnMoreLink: 'http://www.stockholm.se/ByggBo/Leva-Miljovanligt/Klimatsmart-i-hemmet'
       },
       {
@@ -126,7 +126,7 @@ var categories = {
         text: 'Minska energiförbrukningen genom att stänga av TVn och datorn en kväll. Läs, eller samla familj och/eller vänner och spela ett roligt brädspel. Det är klimatsmart och roligt.',
         localOrgText: 'ExampleLocalOrg',
         localOrgLink: ['http://www.example.com/'],
-        learnMoreText: '<h2>Exempel ur utställningen</h2><img src="/make-a-pledge/images/objects/NM.0223731A-B.jpg" height="190" class="learnMoreImage"/><p class="learnMoreText">Nordpolsspelet, tillverkat i Tyskland för svensk publik, 1890-tal. NM.0223731A-B</p>',
+        learnMoreText: '<h2>Exempel ur Nordiska museets samlingar</h2><img src="/make-a-pledge/images/objects/NM.0223731A-B.jpg" height="190" class="learnMoreImage"/><p class="learnMoreText">Nordpolsspelet, tillverkat i Tyskland för svensk publik, 1890-tal. NM.0223731A-B</p>',
         learnMoreLink: 'http://www.stockholm.se/ByggBo/Leva-Miljovanligt/Klimatsmart-i-hemmet'
       }
     ]
@@ -141,7 +141,7 @@ var categories = {
         text: 'Om du låter bli att äta nötkött en gång i veckan under ett år undviker du 150 kilo växthusgasutsläpp. Du sparar även pengar &ndash; på ett år sparar du minst 450 kronor per person i hushållet genom att göra pastasås med bönor istället för med köttfärs.',
         localOrgText: 'ExampleLocalOrg',
         localOrgLink: ['http://www.example.com/'],
-        learnMoreText: '<h2>Exempel ur utställningen</h2><img src="/make-a-pledge/images/objects/NM.0403281.jpg" height="190" class="learnMoreImage"/><p class="learnMoreText">Kornprov från Hämra i Ångermanland, 1896. NM.0403281</p>',
+        learnMoreText: '<h2>Exempel ur Nordiska museets samlingar</h2><img src="/make-a-pledge/images/objects/NM.0403281.jpg" height="190" class="learnMoreImage"/><p class="learnMoreText">Kornprov från Hämra i Ångermanland, 1896. NM.0403281</p>',
         learnMoreLink: 'http://www.stockholm.se/ByggBo/Leva-Miljovanligt/Klimatsmart-i-hemmet'
       },
       {
@@ -150,7 +150,7 @@ var categories = {
         text: 'Varje svensk slänger 28 kilo ätbar mat i soporna varje år. Med planerade matinköp samt rätt förvarade livsmedel kan ditt hushåll spara 3 000–6 000 kr per år. Förvara maten i den temperatur som producenten anger. ”Bäst före-datum” är en rekommendation och maten håller ofta att äta när datum gått ut.',
         localOrgText: 'ExampleLocalOrg',
         localOrgLink: ['http://www.example.com/'],
-        learnMoreText: '<h2>Exempel ur utställningen</h2><img src="/make-a-pledge/images/objects/NM.0311599.jpg" height="190" class="learnMoreImage"/><p class="learnMoreText">Reklamskylt för lingonsylt och lingonsaft från Kalix. NM.0311599</p>',
+        learnMoreText: '<h2>Exempel ur Nordiska museets samlingar</h2><img src="/make-a-pledge/images/objects/NM.0311599.jpg" height="190" class="learnMoreImage"/><p class="learnMoreText">Reklamskylt för lingonsylt och lingonsaft från Kalix. NM.0311599</p>',
         learnMoreLink: 'http://www.stockholm.se/ByggBo/Leva-Miljovanligt/Klimatsmart-i-hemmet'
       },
       {
@@ -159,7 +159,7 @@ var categories = {
         text: 'Fråga vad som är i säsong just nu. Potatis har liksom morot, broccoli och vitkål mycket liten klimatpåverkan eftersom de kan odlas nära och kan lagras länge. Ris har högre klimatpåverkan då det odlas i vatten och växtdelarnas nedbrytning bildar metangas. Ett alternativ till ris är matvete.',
         localOrgText: 'ExampleLocalOrg',
         localOrgLink: ['http://www.example.com/'],
-        learnMoreText: '<h2>Exempel ur utställningen</h2><img src="/make-a-pledge/images/objects/NMA.0062054.jpg" height="190" class="learnMoreImage"/><p class="learnMoreText">Potatisskörd i Åre, Jämtland 1943. Foto Gunnar Lundh. NMA.0062054</p>',
+        learnMoreText: '<h2>Exempel ur Nordiska museets samlingar</h2><img src="/make-a-pledge/images/objects/NMA.0062054.jpg" height="190" class="learnMoreImage"/><p class="learnMoreText">Potatisskörd i Åre, Jämtland 1943. Foto Gunnar Lundh. NMA.0062054</p>',
         learnMoreLink: 'http://www.stockholm.se/ByggBo/Leva-Miljovanligt/Klimatsmart-i-hemmet'
       }
     ]
