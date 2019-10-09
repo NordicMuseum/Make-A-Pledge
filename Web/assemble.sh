c2486e8e51b3cac4e140b2d3355c2a2bc01f686a
Carl Calderon <carl@earthpeople.se>
2019-10-09 10:34:52 +0200

    Add poll assets to the assemble script

--- Web/assemble.sh
+++ Web/assemble.sh
@@ -1,5 +1,10 @@
 # Copy related content
+mkdir ./assets/poll
+mkdir ./assets/poll/images
+mkdir ./assets/poll/fonts
 cp ../Poll/iot/bundle.js ./assets/js/iot.js
+cp -r ../Poll/javascripts/assets/* ./assets/poll/images
+cp -r ../Poll/javascripts/fonts/* ./assets/poll/fonts
 cp ../VotingApplication/data/categories.js ./assets/data/categories.js
 cp ../VotingApplication/data/confirmationSynonyms.js ./assets/data/confirmationSynonyms.js
 cp ../VotingApplication/secrets.js ./assets/data/secrets.js
