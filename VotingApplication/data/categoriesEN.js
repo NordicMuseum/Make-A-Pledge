9c59c9e23f58a2a4799904c6f2a91e65a6a8895b
Aron Ambrosiani <aron@ambrosiani.se>
2019-10-12 00:08:38 +0200

    mobile styles

--- VotingApplication/data/categoriesEN.js
+++ VotingApplication/data/categoriesEN.js
@@ -9,7 +9,7 @@ var categories = {
         text: 'The clothing industry and cotton cultivation are hard on the environment. But if you buy second hand, you can save up to 97 percent of the energy that goes into making new clothes. It is also fun to go to the flea market where you can find really unique garments at low prices.',
         localOrgText: 'ExampleLocalOrg',
         localOrgLink: ['http://www.example.com/'],
-        learnMoreText: '<h2>Examples from the Nordic Museum collections</h2><img src="/make-a-pledge/images/objects/NM.0201803.jpg" height="190" class="learnMoreImage"/><p class="learnMoreText">Shirt from Hälsingland, mid-19th century. Possibly the trunk is taken from an older sweater that has been joined with other woolen fabric on the shoulders. NM.0201803</p>',
+        learnMoreText: '<h2>Examples from the Nordic Museum collections</h2><img src="/make-a-pledge/images/objects/NM.0201803.jpg" class="learnMoreImage"/><p class="learnMoreText">Shirt from Hälsingland, mid-19th century. Possibly the trunk is taken from an older sweater that has been joined with other woolen fabric on the shoulders. NM.0201803</p>',
         learnMoreLink: 'http://www.tidningenhalsa.se/artiklar/kategori/20120420/klimatsmarta-handlar-second-hand/'
       },
       {
@@ -18,7 +18,7 @@ var categories = {
         text: 'Think carefully every time you buy new clothes. If you really need to buy new clothes, then buy fewer garments of better quality that last a long time.',
         localOrgText: 'ExampleLocalOrg',
         localOrgLink: ['http://www.example.com/'],
-        learnMoreText: '<h2>Examples from the Nordic Museum collections</h2><img src="/make-a-pledge/images/objects/NM.0273621A-B.jpg" height="190" class="learnMoreImage"/><p class="learnMoreText">Winter  shoes from Lapland, early 1900s. NM.0273621A-B</p>',
+        learnMoreText: '<h2>Examples from the Nordic Museum collections</h2><img src="/make-a-pledge/images/objects/NM.0273621A-B.jpg" class="learnMoreImage"/><p class="learnMoreText">Winter  shoes from Lapland, early 1900s. NM.0273621A-B</p>',
         learnMoreLink: 'https://extra.orebro.se/klimatsmartvardag/klimatsmartatips/ekonomiochkonsumtion/skapaenhallbargarderob.4.6ad5e21514fe44c621c5044.html'
       },
       {
@@ -27,7 +27,7 @@ var categories = {
         text: 'If a garment breaks, then repair it. A hole in the pants can be patched, the branch can be reinforced and the shirt collar can be turned or replaced. If you can already sew, get started. Otherwise you can take a course or hand in the garment for repair. A patched garment becomes unique and often very beautiful.',
         localOrgText: 'ExampleLocalOrg',
         localOrgLink: ['http://www.example.com/'],
-        learnMoreText: '<h2>Examples from the Nordic Museum collections</h2><img src="/make-a-pledge/images/objects/NM.0240612.jpg" height="190" class="learnMoreImage"/><p class="learnMoreText">Patched logging trousers from Töre in Norrbotten, early 1900s. NM.0240612</p>',
+        learnMoreText: '<h2>Examples from the Nordic Museum collections</h2><img src="/make-a-pledge/images/objects/NM.0240612.jpg" class="learnMoreImage"/><p class="learnMoreText">Patched logging trousers from Töre in Norrbotten, early 1900s. NM.0240612</p>',
         learnMoreLink: 'https://karlavagen31.se/bli-klimatsmart-sa-vardar-du-dina-klader'
 
       }
@@ -43,7 +43,7 @@ var categories = {
         text: 'Whenever possible, take the bike or walk instead of taking the car. It improves your health and saves you money while helping the climate.',
         localOrgText: 'ExampleLocalOrg',
         localOrgLink: ['http://www.example.com/'],
-        learnMoreText: '<h2>Examples from the Nordic Museum collections</h2><img src="/make-a-pledge/images/objects/NM.0328486A-D.jpg" height="190" class="learnMoreImage"/><p class="learnMoreText">Bicycle, manufactured in Stockholm 1960. NM.0328486A-D</p>',
+        learnMoreText: '<h2>Examples from the Nordic Museum collections</h2><img src="/make-a-pledge/images/objects/NM.0328486A-D.jpg" class="learnMoreImage"/><p class="learnMoreText">Bicycle, manufactured in Stockholm 1960. NM.0328486A-D</p>',
         learnMoreLink: 'https://voxnadalen.naturskyddsforeningen.se/gron-guide/res-klimatsmart'
       },
       {
@@ -52,7 +52,7 @@ var categories = {
         text: 'Instead of driving a car, switch to the commuter train, bus or metro.',
         localOrgText: 'ExampleLocalOrg',
         localOrgLink: ['http://www.example.com/'],
-        learnMoreText: '<h2>Examples from the Nordic Museum collections</h2><img src="/make-a-pledge/images/objects/NMA.0058040.jpg" height="190" class="learnMoreImage"/><p class="learnMoreText">Tourist bus in Holmajärvi, Norrbotten, 1946. NMA.0058040</p>',
+        learnMoreText: '<h2>Examples from the Nordic Museum collections</h2><img src="/make-a-pledge/images/objects/NMA.0058040.jpg" class="learnMoreImage"/><p class="learnMoreText">Tourist bus in Holmajärvi, Norrbotten, 1946. NMA.0058040</p>',
         learnMoreLink: 'https://voxnadalen.naturskyddsforeningen.se/gron-guide/res-klimatsmart'
       },
       {
@@ -61,7 +61,7 @@ var categories = {
         text: 'When you go on a longer trip or on vacation, go by train instead of car or plane. The train is comfortable and is often an experience in itself. The trip becomes part of the destination. In addition, you avoid stressful airports, luggage check-in and security checks.',
         localOrgText: 'ExampleLocalOrg',
         localOrgLink: ['http://www.example.com/'],
-        learnMoreText: '<h2>Examples from the Nordic Museum collections</h2><img src="/make-a-pledge/images/objects/NMA.0046474.jpg" height="190" class="learnMoreImage"/><p class="learnMoreText">Lillehammer train station, 1939. Foto: Gunnar Lundh. NMA.0046474</p>',
+        learnMoreText: '<h2>Examples from the Nordic Museum collections</h2><img src="/make-a-pledge/images/objects/NMA.0046474.jpg" class="learnMoreImage"/><p class="learnMoreText">Lillehammer train station, 1939. Foto: Gunnar Lundh. NMA.0046474</p>',
         learnMoreLink: 'https://voxnadalen.naturskyddsforeningen.se/gron-guide/res-klimatsmart'
       }
     ]
@@ -76,7 +76,7 @@ var categories = {
         text: 'Decorate your home climate smart! Do you need furniture or toys for the children, buy second hand. Go to the flea market, check a second hand website or go to an auction. You often get quality furniture for a lower price than new ones of inferior quality.',
         localOrgText: 'ExampleLocalOrg',
         localOrgLink: ['http://www.example.com/'],
-        learnMoreText: '<h2>Examples from the Nordic Museum collections</h2><img src="/make-a-pledge/images/objects/NM.0328242.jpg" height="190" class="learnMoreImage"/><p class="learnMoreText">Butter churn made in Gävle, 1910s. Purchased in used condition by a family in Småland and used during the 1930s and 1940s. NM.0328242</p>',
+        learnMoreText: '<h2>Examples from the Nordic Museum collections</h2><img src="/make-a-pledge/images/objects/NM.0328242.jpg" class="learnMoreImage"/><p class="learnMoreText">Butter churn made in Gävle, 1910s. Purchased in used condition by a family in Småland and used during the 1930s and 1940s. NM.0328242</p>',
         learnMoreLink: 'https://malin.elledecoration.se/att-inreda-hallbart'
       },
       {
@@ -85,7 +85,7 @@ var categories = {
         text: 'Is anything at home broken? Repair it yourself, or submit for repair instead of throwing away and buying new.',
         localOrgText: 'ExampleLocalOrg',
         localOrgLink: ['http://www.example.com/'],
-        learnMoreText: '<h2>Examples from the Nordic Museum collections</h2><img src="/make-a-pledge/images/objects/NM.0115566.jpg" height="190" class="learnMoreImage"/><p class="learnMoreText">Cracked and repaired bowl from Östergötland, before 1910. NM.0115566</p>',
+        learnMoreText: '<h2>Examples from the Nordic Museum collections</h2><img src="/make-a-pledge/images/objects/NM.0115566.jpg" class="learnMoreImage"/><p class="learnMoreText">Cracked and repaired bowl from Östergötland, before 1910. NM.0115566</p>',
         learnMoreLink: 'https://malin.elledecoration.se/att-inreda-hallbart'
       },
       {
@@ -93,7 +93,7 @@ var categories = {
         name: 'Make long-term purchases.',
         text: 'If you really need to buy something new, then choose something of higher quality that lasts longer and is made of environmentally friendly materials. If you buy a sofa, you can check if the upholstery is removable and washable, or if a table can be sanded down.',
         localOrgLink: [],
-        learnMoreText: '<h2>Examples from the Nordic Museum collections</h2><img src="/make-a-pledge/images/objects/NM.0059952.jpg" height="190" class="learnMoreImage"/><p class="learnMoreText">Soapstone lamp from Greenland, using seal fat as fuel, late 19th century. The lamp lasted a long time and was comparatively energy efficient. NM.0059952</p>',
+        learnMoreText: '<h2>Examples from the Nordic Museum collections</h2><img src="/make-a-pledge/images/objects/NM.0059952.jpg" class="learnMoreImage"/><p class="learnMoreText">Soapstone lamp from Greenland, using seal fat as fuel, late 19th century. The lamp lasted a long time and was comparatively energy efficient. NM.0059952</p>',
         learnMoreLink: 'https://malin.elledecoration.se/att-inreda-hallbart'
       }
     ]
@@ -108,7 +108,7 @@ var categories = {
         text: 'Save electricity and heating costs by sealing gaps in walls and floors.',
         localOrgText: 'ExampleLocalOrg',
         localOrgLink: ['http://www.example.com/'],
-        learnMoreText: '<h2>Examples from the Nordic Museum collections</h2><img src="/make-a-pledge/images/objects/NM.0126345.jpg" height="190" class="learnMoreImage"/><p class="learnMoreText">Rana (a woven piece of fabric) from Vittangi in Lapland, before 1915. Fabric pieces have been used to seal walls or to thrive with lower indoor temperature. NM.0126345</p>',
+        learnMoreText: '<h2>Examples from the Nordic Museum collections</h2><img src="/make-a-pledge/images/objects/NM.0126345.jpg" class="learnMoreImage"/><p class="learnMoreText">Rana (a woven piece of fabric) from Vittangi in Lapland, before 1915. Fabric pieces have been used to seal walls or to thrive with lower indoor temperature. NM.0126345</p>',
         learnMoreLink: 'https://www.folksam.se/tester-och-goda-rad/miljo-och-ansvar/bli-klimatsmart/bo-klimatsmart'
       },
       {
@@ -117,7 +117,7 @@ var categories = {
         text: 'When bathing in a tub, 150 liters of water is consumed. The energy use to heat that water is 5.6 kWh. Compare that to taking a 5 minute shower as it can consume 60 liters of water, heated by 2.2 kWh. Treat yourself to a bath sometimes, but only rarely. It benefits both you and the environment.',
         localOrgText: 'ExampleLocalOrg',
         localOrgLink: ['http://www.example.com/'],
-        learnMoreText: '<h2>Examples from the Nordic Museum collections</h2><img src="/make-a-pledge/images/objects/NM.0319074.jpg" height="190" class="learnMoreImage"/><p class="learnMoreText">Bar of soap, manufactured in Nyköping 1970. NM.0319074</p>',
+        learnMoreText: '<h2>Examples from the Nordic Museum collections</h2><img src="/make-a-pledge/images/objects/NM.0319074.jpg" class="learnMoreImage"/><p class="learnMoreText">Bar of soap, manufactured in Nyköping 1970. NM.0319074</p>',
         learnMoreLink: 'http://www.stockholm.se/ByggBo/Leva-Miljovanligt/Klimatsmart-i-hemmet'
       },
       {
@@ -126,7 +126,7 @@ var categories = {
         text: 'Reduce energy consumption by turning off the TV and computer one evening. Read, or gather family and/or friends and play a fun board game. It is climate smart and fun.',
         localOrgText: 'ExampleLocalOrg',
         localOrgLink: ['http://www.example.com/'],
-        learnMoreText: '<h2>Examples from the Nordic Museum collections</h2><img src="/make-a-pledge/images/objects/NM.0223731A-B.jpg" height="190" class="learnMoreImage"/><p class="learnMoreText">The North Pole Game, made in Germany for Swedish audiences, 1890s. NM.0223731A-B</p>',
+        learnMoreText: '<h2>Examples from the Nordic Museum collections</h2><img src="/make-a-pledge/images/objects/NM.0223731A-B.jpg" class="learnMoreImage"/><p class="learnMoreText">The North Pole Game, made in Germany for Swedish audiences, 1890s. NM.0223731A-B</p>',
         learnMoreLink: 'http://www.stockholm.se/ByggBo/Leva-Miljovanligt/Klimatsmart-i-hemmet'
       }
     ]
@@ -141,7 +141,7 @@ var categories = {
         text: 'If you refrain from eating beef once a week for one year, you avoid 150 kilos of greenhouse gas emissions. You also save money – in a year you save at least SEK 450 per person in the household by making pasta sauce with beans instead of minced meat.',
         localOrgText: 'ExampleLocalOrg',
         localOrgLink: ['http://www.example.com/'],
-        learnMoreText: '<h2>Examples from the Nordic Museum collections</h2><img src="/make-a-pledge/images/objects/NM.0403281.jpg" height="190" class="learnMoreImage"/><p class="learnMoreText">Grain sample from Hämra in Ångermanland, 1896. NM.0403281</p>',
+        learnMoreText: '<h2>Examples from the Nordic Museum collections</h2><img src="/make-a-pledge/images/objects/NM.0403281.jpg" class="learnMoreImage"/><p class="learnMoreText">Grain sample from Hämra in Ångermanland, 1896. NM.0403281</p>',
         learnMoreLink: 'http://www.stockholm.se/ByggBo/Leva-Miljovanligt/Klimatsmart-i-hemmet'
       },
       {
@@ -150,7 +150,7 @@ var categories = {
         text: 'Each Swede throws 28 kilos of edible food in the garbage every year. With planned food purchases and properly stored food, your household can save SEK 3,000–6,000 per year. Store the food at the temperature specified by the manufacturer. The “Best before date” is a recommendation and the food is often edible even when the expiry date has passed.',
         localOrgText: 'ExampleLocalOrg',
         localOrgLink: ['http://www.example.com/'],
-        learnMoreText: '<h2>Examples from the Nordic Museum collections</h2><img src="/make-a-pledge/images/objects/NM.0311599.jpg" height="190" class="learnMoreImage"/><p class="learnMoreText">Advertising sign for lingonberry jam and lingonberry juice from Kalix. NM.0311599</p>',
+        learnMoreText: '<h2>Examples from the Nordic Museum collections</h2><img src="/make-a-pledge/images/objects/NM.0311599.jpg" class="learnMoreImage"/><p class="learnMoreText">Advertising sign for lingonberry jam and lingonberry juice from Kalix. NM.0311599</p>',
         learnMoreLink: 'http://www.stockholm.se/ByggBo/Leva-Miljovanligt/Klimatsmart-i-hemmet'
       },
       {
@@ -159,7 +159,7 @@ var categories = {
         text: 'Ask what’s in season right now. Potatoes, just like carrot, broccoli and white cabbage, have a small climate impact because they can be grown locally and can be stored for a long time. Rice has a higher climate impact as it is grown in water and the decomposition of plant parts forms methane gas. An alternative to rice is wheat berries.',
         localOrgText: 'ExampleLocalOrg',
         localOrgLink: ['http://www.example.com/'],
-        learnMoreText: '<h2>Examples from the Nordic Museum collections</h2><img src="/make-a-pledge/images/objects/NMA.0062054.jpg" height="190" class="learnMoreImage"/><p class="learnMoreText">Potato harvest in Åre, Jämtland 1943. Foto Gunnar Lundh. NMA.0062054</p>',
+        learnMoreText: '<h2>Examples from the Nordic Museum collections</h2><img src="/make-a-pledge/images/objects/NMA.0062054.jpg" class="learnMoreImage"/><p class="learnMoreText">Potato harvest in Åre, Jämtland 1943. Foto Gunnar Lundh. NMA.0062054</p>',
         learnMoreLink: 'http://www.stockholm.se/ByggBo/Leva-Miljovanligt/Klimatsmart-i-hemmet'
       }
     ]
